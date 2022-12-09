22106e235afd888659a3bad4c0dde3476a655e9a
AncuL001 <ancul001@gmail.com>
2022-12-09 21:36:32 +0700

    move method implementation to base class

--- src/appliable_mode_handler/appliable_mode_handler.hpp
+++ src/appliable_mode_handler/appliable_mode_handler.hpp
@@ -18,9 +18,6 @@ protected:
     // Apparently this variable gets destroyed on class constructor call???
     std::unique_ptr<PointGenerator> pointGenerator;
 
-public:
-    Point endPoint;
-
     AppliableModeHandler(Grid& grid, Point startingPoint, Colors::Color lineColor) : 
         grid(grid),
         startingPoint(startingPoint),
@@ -28,6 +25,30 @@ public:
         lineColor(lineColor)
         {}
 
-    virtual void renderPreview() = 0;
-    virtual void apply() = 0;
+public:
+    Point endPoint;
+
+    void renderPreview() {
+        GLfloat gridWidth = GLfloat(glutGet(GLUT_WINDOW_WIDTH)) / grid.grid[0].size();
+        GLfloat gridHeight = GLfloat(glutGet(GLUT_WINDOW_HEIGHT)) / grid.grid.size();
+
+        for (const auto& point: pointGenerator->generate(startingPoint, endPoint)) {
+            GLfloat startX = point.x * gridWidth;
+            GLfloat startY = point.y * gridHeight;
+
+            colorHex(lineColor.color);
+            glBegin(GL_POLYGON);
+                glVertex2f(startX, startY);
+                glVertex2f(startX, startY + gridHeight);
+                glVertex2f(startX + gridWidth, startY + gridHeight);
+                glVertex2f(startX + gridWidth, startY);
+            glEnd();
+        }
+    };
+
+    virtual void apply() {
+        for (const auto& point: pointGenerator->generate(startingPoint, endPoint)) {
+            grid.grid[point.y][point.x] = lineColor.color;
+        }
+    };
 };
--- src/appliable_mode_handler/line_mode_handler.hpp
+++ src/appliable_mode_handler/line_mode_handler.hpp
@@ -11,28 +11,4 @@ public:
         {
             pointGenerator = std::make_unique<BresenhamPointGenerator>(BresenhamPointGenerator());
         }
-
-    void renderPreview() override {
-        GLfloat gridWidth = GLfloat(glutGet(GLUT_WINDOW_WIDTH)) / grid.grid[0].size();
-        GLfloat gridHeight = GLfloat(glutGet(GLUT_WINDOW_HEIGHT)) / grid.grid.size();
-
-        for (const auto& point: pointGenerator->generate(startingPoint, endPoint)) {
-            GLfloat startX = point.x * gridWidth;
-            GLfloat startY = point.y * gridHeight;
-
-            colorHex(lineColor.color);
-            glBegin(GL_POLYGON);
-                glVertex2f(startX, startY);
-                glVertex2f(startX, startY + gridHeight);
-                glVertex2f(startX + gridWidth, startY + gridHeight);
-                glVertex2f(startX + gridWidth, startY);
-            glEnd();
-        }
-    }
-
-    void apply() override {
-        for (const auto& point: pointGenerator->generate(startingPoint, endPoint)) {
-            grid.grid[point.y][point.x] = lineColor.color;
-        }
-    }
 };
